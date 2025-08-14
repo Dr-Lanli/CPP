b32dd1134cce3267b84e5f04a7c380abea6c617b
Lilian <lilianmon57@gmail.com>
2025-08-14 19:13:12 +0200

    update

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -3,30 +3,28 @@
 PhoneBook::PhoneBook() {}
 PhoneBook::~PhoneBook() {}
 
-
 int parsing_add_input(PhoneBook &book, Contact contact[8], std::string input[5])
 {
+
     for (size_t i = 0; i < 5; ++i)
     {
         if (input[i] == "")
         {
             std::cout << "field cannot be empty, contact not saved" << std::endl;
-            std::cout << "Please write a command: (ADD, SEARCH, EXIT)" << std::endl;
-            std::getline(std::cin, book.command);
-            return (book.parsing(book, contact));
+            return (1); 
         }
     }
+    
+
     for (size_t i = 0; i < input[3].size(); i++)
     {
         if (!std::isdigit(input[3][i]) && input[3][i] != '+')
         {
             std::cout << "phone number is not coherent, contact not saved" << std::endl;
-            std::cout << "Please write a command: (ADD, SEARCH, EXIT)" << std::endl;
-			std::getline(std::cin, book.command);
-			return (book.parsing(book, contact));
+            return (1); 
         }
     }
-    return (0);
+    return (0); 
 }
 
 void add_cmd(PhoneBook &book, Contact contact[8])
@@ -46,100 +44,110 @@ void add_cmd(PhoneBook &book, Contact contact[8])
         std::getline(std::cin, inputs[i]);
     }
 
-    parsing_add_input(book, contact, inputs);
-	if (book.nb_contacts == 8)
-		book.nb_contacts = 0;
-    contact[book.nb_contacts++].setting_inputs(inputs);
-	std::cout << "add_cmd: " << book.nb_contacts << std::endl;
+    if (parsing_add_input(book, contact, inputs) == 0)
+    {
+        if (book.nb_contacts == 8)
+            book.nb_contacts = 0;
+        contact[book.nb_contacts++].setting_inputs(inputs);
+        std::cout << "Contact added successfully!" << std::endl;
+    }
 }
 
 void list_contacts(PhoneBook &book, Contact contact[8])
 {
-    int i = 0;
-	std::string inputs[3];
+    std::string inputs[3];
 
     std::cout << "INDEX     |FIRST NAME|LAST NAME |NICKNAME  " << std::endl;
-	for (int i = 0; i < MAX_CONTACT; i++)
-	{
-		contact[i].getting_name_inputs(inputs);
-		if (inputs[0].empty())
-			break;
-		std::cout << std::setw(10) << std::right << i;
-		for (int j = 0; j < 3; j++)
-		{
-			std::string truncated = inputs[j];
-			if (truncated.length() > 10) 
-				truncated = truncated.substr(0, 9) + ".";
-			std::cout << "|" << std::setw(10) << std::right << truncated;
-		}
-		std::cout << std::endl;
-	}
+    std::cout << "----------|----------|----------|----------" << std::endl;
+    
+    for (int i = 0; i < MAX_CONTACT; i++)
+    {
+        contact[i].getting_name_inputs(inputs);
+        if (inputs[0].empty())
+            break;
+        std::cout << std::setw(10) << std::right << i;
+        for (int j = 0; j < 3; j++)
+        {
+            std::string truncated = inputs[j];
+            if (truncated.length() > 10) 
+                truncated = truncated.substr(0, 9) + ".";
+            std::cout << "|" << std::setw(10) << std::right << truncated;
+        }
+        std::cout << std::endl;
+    }
 }
 
 void search_cmd_index(PhoneBook &book, Contact contact[8], int index)
 {
-	std::string inputs[5];
+    std::string inputs[5];
 
-	if (index > book.nb_contacts)
-	{
-		std::cout << "contact: " << index << " doesn't exist" << std::endl;
-		return ;
-	}
-	contact[index].getting_all_inputs(inputs);
-	std::cout << "FirstName: " << inputs[0] << std::endl;
-	std::cout << "LastName: " << inputs[1] << std::endl;
-	std::cout << "NickName: " << inputs[2] << std::endl;
-	std::cout << "PhoneNumber: " << inputs[3] << std::endl;
-	std::cout << "DarkSecret: " << inputs[4] << std::endl;
+    if (index >= book.nb_contacts || index < 0)
+    {
+        std::cout << "contact: " << index << " doesn't exist" << std::endl;
+        return ();
+    }
+    
+    contact[index].getting_all_inputs(inputs);
+    std::cout << "FirstName: " << inputs[0] << std::endl;
+    std::cout << "LastName: " << inputs[1] << std::endl;
+    std::cout << "NickName: " << inputs[2] << std::endl;
+    std::cout << "PhoneNumber: " << inputs[3] << std::endl;
+    std::cout << "DarkSecret: " << inputs[4] << std::endl;
 }
 
 int search_index(PhoneBook &book, Contact contact[8])
 {
-	int index = 0;
-	int flag = 0;
+    int index = 0;
+    int flag = 0;
+    bool valid_input = true;
 
-	index = book.command[0] - '0';
-	for (size_t j = 0; j < book.command.length(); j++)
-	{
-		if (std::isdigit(book.command[j]))
-		{
-			if (++flag > 1 
-				|| !std::isdigit(book.command[j]) 
-				|| (book.command[j] - '0') < 0 
-				|| (book.command[j] - '0') > 7)
-			{
-				std::cout << "SEARCH index is not coherent" << std::endl;
-				std::cout << "must be a digit between 0 to 7" << std::endl;
-				return(book.search_cmd(book, contact));
-			}
-			//std::cout << "index :" << index << std::endl;
-		}
-		else
-		{
-			std::cout << "SEARCH index is not coherent" << std::endl;
-			std::cout << "must be a digit between 0 to 7" << std::endl;
-			return(book.search_cmd(book, contact));
-		}
-		
-	}
-	return (index);
+            if (book.command.length() != 1)
+        {
+            std::cout << "SEARCH index is not coherent" << std::endl;
+            std::cout << "must be a digit between 0 to 7" << std::endl;
+            return (-1); 
+        }
+
+            if (!std::isdigit(book.command[0]))
+        {
+            std::cout << "SEARCH index is not coherent" << std::endl;
+            std::cout << "must be a digit between 0 to 7" << std::endl;
+            return (-1);
+        }
+
+    index = book.command[0] - '0';
+    
+
+            if (index < 0 || index > 7)
+        {
+            std::cout << "SEARCH index is not coherent" << std::endl;
+            std::cout << "must be a digit between 0 to 7" << std::endl;
+            return (-1);
+        }
+
+    return (index);
 }
 
 int PhoneBook::search_cmd(PhoneBook &book, Contact contact[8])
 {
-	std::string inputs[3];
-	int index = 0;
+    std::string inputs[3];
+    int index = 0;
 
-	list_contacts(book, contact);
-	contact[0].getting_name_inputs(inputs);
-	if (inputs[0] != "")
-	{
-		std::cout << "Enter a contact index:" << std::endl;
-		std::getline(std::cin, book.command);
-		index = search_index(book, contact);
-		search_cmd_index(book, contact, index);
-	}
-	return (0);
+    list_contacts(book, contact);
+    contact[0].getting_name_inputs(inputs);
+    
+    if (inputs[0] != "")
+    {
+        std::cout << "Enter a contact index:" << std::endl;
+        std::getline(std::cin, book.command);
+        index = search_index(book, contact);
+        
+        if (index != -1)
+        {
+            search_cmd_index(book, contact, index);
+        }
+    }
+    return (0);
 }
 
 int PhoneBook::parsing(PhoneBook &book, Contact contact[8])
@@ -150,7 +158,7 @@ int PhoneBook::parsing(PhoneBook &book, Contact contact[8])
     }
     else if (book.command == "SEARCH")
     {
-		return (search_cmd(book, contact));
+        return search_cmd(book, contact);
     }
     else if (book.command == "EXIT")
     {
@@ -160,9 +168,7 @@ int PhoneBook::parsing(PhoneBook &book, Contact contact[8])
     {
         std::cout << "Please write a command: (ADD, SEARCH, EXIT)" << std::endl;
         std::getline(std::cin, book.command);
-        return (parsing(book, contact));
+        return (0);
     }
     return (0);
 }
-
-/* PROBLEME DE CREATION DE CONTACT LORS D'ERREURS */
