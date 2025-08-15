656806d852ef512b1b6c4064ff5d30ff16dbb3ae
Lilian <lilianmon57@gmail.com>
2025-08-15 16:31:25 +0200

    ex02 finished

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -1,10 +1,19 @@
-#include "Zombie.hpp"
+#include <iostream>
+#include <string.h>
 
 int main (void)
 {
-    Zombie tmp;
+    std::string brain = "HI THIS IS BRAIN";
+	std::string *stringPTR = &brain;
+	std::string &stringREF = brain;
 
-	
+	std::cout << "memory address of 'brain' variable: " << &brain << std::endl;
+	std::cout << "memory address of 'stringPTR' variable: " << &stringPTR << std::endl;
+	std::cout << "memory address of 'stringREF' variable: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "value of 'brain' variable: " << brain << std::endl;
+	std::cout << "value of 'stringPTR' variable: " << stringPTR << std::endl;
+	std::cout << "value of 'stringREF' variable: " << stringREF << std::endl;
 
     return (0);
 }
